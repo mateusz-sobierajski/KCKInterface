4e9828b70eb6527c0d93908c21572ca56fc7518b
Mateusz Sobierajski <mateusz.sobierajski.official@gmail.com>
2023-09-24 21:11:54 +0200

    Doprecyzowanie ekranów pomocy

--- Screens/MainTab/HelpID.js
+++ Screens/MainTab/HelpID.js
@@ -1,14 +1,27 @@
-import {Text, View} from "react-native";
+import {Text, TextInput, View} from "react-native";
 import {WelcomeStyles} from "../../Styles/WelcomeStyles";
 import {ScreenStyles} from "../../Styles/ScreenStyles";
-//import MyButton from "../../Elements/MyButton";
+import React from "react";
+import {DeviceChoiceStyles} from "../../Styles/DeviceChoiceStyles";
+import MyButton from "../../Elements/MyButton";
 
 const HelpID = () => {
     return (
         <View style={WelcomeStyles.welcomeScreen}>
-            <Text style={ScreenStyles.mainText}>Do pola ID należy wpisać ID danego systemu</Text>
+            <Text style={ScreenStyles.mainText}>Do pola</Text>
+            <TextInput
+                style={DeviceChoiceStyles.textInput}
+                placeholder='ID:'
+                editable={false}
+            />
+            <Text/>
+            <Text style={ScreenStyles.mainText}>należy wpisać ID danego systemu</Text>
+
             <Text></Text>
-            <Text style={ScreenStyles.mainText}>Po wpisaniu ID należy zatwierdzić przyciskiem "Zatwierdź"</Text>
+
+            <Text style={ScreenStyles.mainText}>Po wpisaniu ID należy zatwierdzić przyciskiem</Text>
+            <MyButton style={WelcomeStyles.helloButton}> Zatwierdź </MyButton>
+            <Text/>
         </View>
     );
 }
--- Screens/MainTab/HelpLogin.js
+++ Screens/MainTab/HelpLogin.js
@@ -1,14 +1,23 @@
 import {Text, View} from "react-native";
 import {WelcomeStyles} from "../../Styles/WelcomeStyles";
 import {ScreenStyles} from "../../Styles/ScreenStyles";
-//import MyButton from "../../Elements/MyButton";
+import MyButton from "../../Elements/MyButton";
+import React from "react";
 
 const HelpLogin = () => {
     return (
         <View style={WelcomeStyles.welcomeScreen}>
-            <Text style={ScreenStyles.mainText}> Przycisk "Start" otwiera ekran wyboru urządzenia</Text>
-            <Text></Text>
-            <Text style={ScreenStyles.mainText}>Przycisk "Ustawienia" nawiguje do ekranu ustawień</Text>
+            <Text style={ScreenStyles.mainText}>Przycisk</Text>
+            <MyButton style={WelcomeStyles.helloButton}> START </MyButton>
+            <Text/>
+            <Text style={ScreenStyles.mainText}>otwiera ekran wyboru urządzenia</Text>
+
+            <Text/>
+
+            <Text style={ScreenStyles.mainText}>Przycisk</Text>
+            <MyButton style={WelcomeStyles.helloButton}> USTAWIENIA </MyButton>
+            <Text/>
+            <Text style={ScreenStyles.mainText}>nawiguje do ekranu ustawień</Text>
         </View>
     );
 }
